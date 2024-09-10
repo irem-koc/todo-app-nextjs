1e2e83617d4c175cf666a7c7afbcfc90deda1c15
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-09-10 15:30:01 +0300

    feat: Display todos in the Home page
    This commit adds the functionality to display todos in the Home page. It imports the `useRootContext` hook from the `root` context and uses it to access the `todos` state. The todos are then mapped and rendered in the page, with each todo item having a checkbox and a description.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+"use client";
 import AddTodo from "@/components/AddTodo";
+import { useRootContext } from "@/context/root";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const { todos } = useRootContext();
   return (
     <div className={styles.page}>
       <AddTodo />
+      {todos.map((todo) => (
+        <div key={todo.id} className={styles.todo}>
+          <input type="checkbox" defaultChecked={todo.completed} />
+          <p>{todo.todo}</p>
+        </div>
+      ))}
     </div>
   );
 }
