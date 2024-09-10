24c40877ba6c6157929ebcf692dbbadabf5d9338
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-09-10 15:30:15 +0300

    feat: Add functionality to add new todos
    This commit adds functionality to the AddTodo component to allow users to add new todos. The handleSubmit function now captures the form data and adds a new todo to the list using the addTodo function from the RootContextProvider. The form is then reset after the todo is added.
    
    Refactor the code to remove unnecessary console.log statements and improve code readability.

--- components/AddTodo.tsx
+++ components/AddTodo.tsx
@@ -1,12 +1,21 @@
 "use client";
+import { useRootContext } from "@/context/root";
 import { FormEvent } from "react";
 import styles from "./addTodo.module.css";
 
 const AddTodo = () => {
+  const { addTodo } = useRootContext();
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    console.log(formData.get("name"), "formdata");
+    console.log(formData.get("name"), "vformData.ge");
+
+    addTodo({
+      id: Math.random().toString(36).substr(2, 9),
+      todo: formData.get("name") as string,
+      completed: false,
+    });
+    event.currentTarget.reset();
   };
   return (
     <form onSubmit={handleSubmit} className={styles.mainContainer}>
