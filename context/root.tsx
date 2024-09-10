14428b1794d9569980f22d6ec4f24be6d82e3060
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-09-10 15:30:21 +0300

    feat: Add todo functionality to RootContextProvider
    - Add Todo and Todos types to import
    - Add todos state and related functions to RootContextProvider
    - Implement addTodo function to add new todos
    - Implement editTodo function to update existing todos

--- context/root.tsx
+++ context/root.tsx
@@ -1,15 +1,36 @@
 "use client";
 
-import { createContext, ReactNode, useContext } from "react";
+import { Todo, Todos } from "@/types";
+import { createContext, ReactNode, useContext, useState } from "react";
 
-interface Context {}
+interface Context {
+  todos: Todos;
+  editTodo: (todo: Todo) => void;
+  addTodo: (todo: Todo) => void;
+}
+
 interface Props {
   children: Readonly<ReactNode>;
 }
-const initialState: Context = {};
+
+const initialState: Context = {
+  todos: [],
+  editTodo: () => {},
+  addTodo: () => {},
+};
+
 export const RootContext = createContext<Context>(initialState);
+
 export const useRootContext = () => useContext(RootContext);
+
 export default function RootContextProvider({ children }: Props) {
-  const values = {};
+  const [todos, setTodos] = useState<Todos>(initialState.todos);
+  const addTodo = (todo: Todo) => {
+    setTodos([...todos, todo]);
+  };
+  const editTodo = (editTodo: Todo) => {
+    setTodos(todos.map((todo) => (todo.id === editTodo.id ? editTodo : todo)));
+  };
+  const values = { todos, addTodo, editTodo };
   return <RootContext.Provider value={values}>{children}</RootContext.Provider>;
 }
