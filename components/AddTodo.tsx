9018e46c5c6354e4c90e6339937c0d0b72cb250d
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-09-12 21:41:55 +0300

    Refactor AddTodo component: Update form styling

--- components/AddTodo.tsx
+++ components/AddTodo.tsx
@@ -1,23 +1,30 @@
 "use client";
 import { useRootContext } from "@/context/root";
 import { FormEvent } from "react";
-
+import styles from "./style.module.css";
 const AddTodo = () => {
   const { addTodo } = useRootContext();
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     addTodo({
-      id: Math.random().toString(36).substr(2, 9),
+      id: crypto.randomUUID(),
       todo: formData.get("name") as string,
       completed: false,
     });
     event.currentTarget.reset();
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="name" placeholder="Enter todo.." />
-      <button type="submit">Add</button>
+    <form className={styles.formContainer} onSubmit={handleSubmit}>
+      <input
+        className={styles.input}
+        type="text"
+        name="name"
+        placeholder="Enter todo.."
+      />
+      <button className={styles.button} type="submit">
+        Add
+      </button>
     </form>
   );
 };
