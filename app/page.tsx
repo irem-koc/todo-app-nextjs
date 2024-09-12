28bd749cf0de313fc707637dc9a53e29827e01c5
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-09-12 21:41:49 +0300

    Refactor Home component: Add editTodo and completeTodo functions
    - Add editTodo function to handle editing todos
    - Add completeTodo function to handle marking todos as completed
    - Update todo rendering to show completed todos with strikethrough
    - Update todo operations icons to include edit functionality
    - Update todo operations icons to include delete functionality
    - Add task count at the bottom of the todo container

--- app/page.tsx
+++ app/page.tsx
@@ -4,33 +4,49 @@ import { useRootContext } from "@/context/root";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const { todos, deleteTodo } = useRootContext();
+  const { todos, deleteTodo, editTodo, completeTodo } = useRootContext();
   const handleDelete = (id: string) => {
     deleteTodo(id);
+  };
+  const handleEdit = (id: string, todo: string) => {
+    editTodo(id, todo);
+  };
+  const handleCompleteTodo = (id: string) => {
+    completeTodo(id);
   };
   return (
     <div className={styles.page}>
       <AddTodo />
-      <div className={styles.todoContainer}></div>
-      {todos.map((todo) => (
-        <div key={todo.id} className={styles.todo}>
-          <div>
-            <input type="checkbox" defaultChecked={todo.completed} />
-            <span>{todo.todo}</span>
+      <div className={styles.todoContainer}>
+        {todos.map((todo) => (
+          <div key={todo.id} className={styles.todo}>
+            <div>
+              <input
+                type="checkbox"
+                onClick={() => handleCompleteTodo(todo.id)}
+                defaultChecked={todo.completed}
+              />
+              {todo.completed ? <s>{todo.todo}</s> : <span>{todo.todo}</span>}
+            </div>
+            <div className={styles.operations}>
+              <span
+                className={styles.hoverPointer}
+                onClick={() => handleEdit(todo.id, "todo")}
+              >
+                <i className="fa-solid fa-pen-to-square"></i>
+              </span>
+              <span
+                className={styles.hoverPointer}
+                onClick={() => handleDelete(todo.id)}
+              >
+                <i className="fa-regular fa-trash-can"></i>
+              </span>
+            </div>
           </div>
-          <div className={styles.operations}>
-            <span className={styles.hoverPointer}>
-              <i className="fa-solid fa-pen-to-square"></i>
-            </span>
-            <span
-              className={styles.hoverPointer}
-              onClick={() => handleDelete(todo.id)}
-            >
-              <i className="fa-regular fa-trash-can"></i>
-            </span>
-          </div>
-        </div>
-      ))}
+        ))}
+        <hr />
+        {todos?.length} görev sayısı.
+      </div>
     </div>
   );
 }
